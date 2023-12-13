264724a99d8a256c0957d9b7d435f5203a2c6062
HP <deborah.oyinda@yahoo.com>
2023-12-13 22:03:10 +0100

    More background change

--- app/src/main/java/com/debbie/happy_birthday/MainActivity.kt
+++ app/src/main/java/com/debbie/happy_birthday/MainActivity.kt
@@ -70,7 +70,7 @@ fun GreetingText(message: String, from: String,modifier: Modifier = Modifier){
 }
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier= Modifier){
-    val image = painterResource(R.drawable.bosun_1)
+    val image = painterResource(R.drawable.biodola_3)
     Box{
         Image( painter = image,
             contentDescription = null,
